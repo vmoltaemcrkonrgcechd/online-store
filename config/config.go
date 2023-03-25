4bcf112a207688ed41ac6a2553bf23020ea3f842
vmoltaemcrkonrgcechd <vmoltaemcrkonrgcechd@gmail.com>
2023-03-25 17:04:54 +0700

    added config

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	HTTPAddr string `yaml:"httpAddr"`
+	PgURL    string `env:"PG_URL"`
+}
+
+func New() (*Config, error) {
+	cfg := new(Config)
+
+	err := cleanenv.ReadConfig("./config/config.yaml", cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	if err = cleanenv.ReadEnv(cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
