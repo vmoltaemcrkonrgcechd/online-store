6acc37112d40b723e98d67f2fd2d08a703afc8b6
vmoltaemcrkonrgcechd <vmoltaemcrkonrgcechd@gmail.com>
2023-03-26 12:10:17 +0700

    described the user

---
+++ internal/entities/user.go
@@ -0,0 +1,17 @@
+package entities
+
+type User struct {
+	ID        string  `json:"id"`
+	Username  string  `json:"username"`
+	City      City    `json:"city"`
+	ImagePath *string `json:"imagePath"`
+	Role      string  `json:"role"`
+}
+
+type UserDTO struct {
+	Username  string  `json:"username"`
+	Password  string  `json:"password"`
+	CityID    string  `json:"cityID"`
+	ImagePath *string `json:"imagePath"`
+	Role      string  `json:"role"`
+}
