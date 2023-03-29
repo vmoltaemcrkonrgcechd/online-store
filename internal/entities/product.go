707a706a5dfb7710e0004cdd8ece31c12aac7950
vmoltaemcrkonrgcechd <vmoltaemcrkonrgcechd@gmail.com>
2023-03-29 18:29:47 +0700

    described the product

---
+++ internal/entities/product.go
@@ -0,0 +1,27 @@
+package entities
+
+type Product struct {
+	ID           string   `json:"id"`
+	Name         string   `json:"name"`
+	User         User     `json:"user"`
+	Color        Color    `json:"color"`
+	Category     Category `json:"category"`
+	UnitPrice    uint     `json:"unitPrice"`
+	UnitsInStock uint     `json:"unitsInStock"`
+}
+
+type ProductDTO struct {
+	Name         string   `json:"name"`
+	UserID       string   `json:"userID"`
+	ColorID      string   `json:"colorID"`
+	CategoryID   string   `json:"categoryID"`
+	UnitPrice    int      `json:"unitPrice"`
+	UnitsInStock int      `json:"unitsInStock"`
+	ImagePaths   []string `json:"imagePaths"`
+}
+
+type AllProductsDTO struct {
+	Products []Product `json:"products"`
+
+	Quantity uint16 `json:"quantity"`
+}
